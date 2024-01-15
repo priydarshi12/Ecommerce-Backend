e77528abe0fa4f7ef17e256f7094df9c9e6f6d46
priydarshi12 <21001016054.jcboseust.ac.in>
2024-01-15 17:19:08 +0530

    CRUD controllers basic implementation completed

--- controllers/category.js
+++ controllers/category.js
@@ -10,7 +10,44 @@ exports.create = async (req, res) => {
     res.status(400).send("create category failed");
   }
 };
-exports.list = (req, res) => {};
-exports.read = (req, res) => {};
-exports.update = (req, res) => {};
-exports.remove = (req, res) => {};
+exports.list = async (req, res) => {
+  try {
+    const categories = await Category.find({}).sort({ createdAt: -1 });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch categories" });
+  }
+};
+
+exports.read = async (req, res) => {
+  try {
+    const category = await Category.findOne({ slug: req.params.slug });
+    res.json(category);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch categories" });
+  }
+};
+exports.update = async (req, res) => {
+   
+  try {
+
+    const { name } = req.body;
+    console.log(name);
+    const updated = await Category.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ error: "Delete Failed" });
+  }
+};
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+    res.json(deleted);
+  } catch (err) {
+    res.status(500).json({ error: "Delete Failed" });
+  }
+};
--- models/category.js
+++ models/category.js
@@ -1,20 +1,23 @@
+
+
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-    name:{
-        type:"String",
-        trim :true,
-        required:"Name is required",
-        minlength:[2,"Too short"],
-        maxlength:[32,"Too long"],
+    name: {
+        type: String,
+        trim: true,
+        required: "Name is required",
+        minlength: [2, "Too short"],
+        maxlength: [32, "Too long"],
     },
-    slug:{
-        type:"String",
-        unique:true,
-        lowercase:true,
-        index:true,
+    slug: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        index: true,
     }
-}
-);
+}, {
+    timestamps: true // This will add createdAt and updatedAt fields
+});
 
-module.exports=mongoose.model('Category',categorySchema);
+module.exports = mongoose.model('Category', categorySchema);
