4cd4b2c7e075208d818b68fc55152840560714cc
priydarshi12 <21001016054.jcboseust.ac.in>
2024-01-15 11:04:09 +0530

    category created

--- controllers/category.js
+++ controllers/category.js
@@ -1,8 +1,16 @@
-const Category=require("../models/category")
+const Category = require("../models/category");
+const slugify = require("slugify");
 
-
-exports.create=(req,res)=>{}
-exports.list=(req,res)=>{}
-exports.read=(req,res)=>{}
-exports.update=(req,res)=>{}
-exports.remove=(req,res)=>{}
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await new Category({ name, slug: slugify(name) }).save();
+    res.json(category);
+  } catch (err) {
+    res.status(400).send("create category failed");
+  }
+};
+exports.list = (req, res) => {};
+exports.read = (req, res) => {};
+exports.update = (req, res) => {};
+exports.remove = (req, res) => {};
