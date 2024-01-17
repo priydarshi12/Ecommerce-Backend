fd2c647299bd63f211cddca55dd1c9ddced4bd79
priydarshi12 <21001016054.jcboseust.ac.in>
2024-01-17 20:12:32 +0530

    changes in update controller

--- controllers/sub.js
+++ controllers/sub.js
@@ -2,11 +2,13 @@ const Sub = require("../models/subcategory");
 const slugify = require("slugify");
 
 exports.create = async (req, res) => {
-  try {
-    const { name } = req.body;
-    const sub = await new Sub({ name, slug: slugify(name) }).save();
+  try {console.log("req body for sub category->",req.body);
+
+    const { name, parent } = req.body;
+    const sub = await new Sub({ name, parent, slug: slugify(name) }).save();
     res.json(sub);
   } catch (err) {
+    console.log(err)
     res.status(400).send("create sub failed");
   }
 };
@@ -28,18 +30,18 @@ exports.read = async (req, res) => {
   }
 };
 exports.update = async (req, res) => {
-   
   try {
-    console.log("update request body",req.body);
-    const { name } = req.body;
+    console.log("update request body---------------", req.body);
+    const { name,parent } = req.body;
     console.log(name);
     const updated = await Sub.findOneAndUpdate(
       { slug: req.params.slug },
-      { name, slug: slugify(name) },
+      { name,parent, slug: slugify(name) },
       { new: true }
     );
     res.json(updated);
   } catch (err) {
+    console.log("error in update->",err.message);
     res.status(500).json({ error: "Update Failed" });
   }
 };
