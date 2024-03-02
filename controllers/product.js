437bc8f05e6ba647e66afa49520449ac77ca6f6d
priydarshi12 <21001016054.jcboseust.ac.in>
2024-03-02 17:08:13 +0530

    single product implementation complete

--- controllers/product.js
+++ controllers/product.js
@@ -61,16 +61,38 @@ exports.update = async (req, res) => {
 };
 
 
+// exports.list = async (req, res) => {
+//   try {
+//     // createdAt/updatedAt, desc/asc, 3
+//     const { sort, order, limit } = req.body;
+//     const products = await Product.find({})
+//       .populate("category")
+//       .populate("subs")
+//       .sort([[sort, order]])
+//       .limit(limit)
+      
+
+//     res.json(products);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+//pagination
 exports.list = async (req, res) => {
   try {
     // createdAt/updatedAt, desc/asc, 3
-    const { sort, order, limit } = req.body;
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 3; // 3
+
     const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
       .populate("category")
       .populate("subs")
       .sort([[sort, order]])
-      .limit(limit)
-      
+      .limit(perPage)
+      .exec();
 
     res.json(products);
   } catch (err) {
