e7f7f7fc5dad4d58d9c14bd86e09305c428e24f5
priydarshi12 <21001016054.jcboseust.ac.in>
2024-03-04 14:51:30 +0530

    star rating implementation

--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controller
-const { create,listAll,remove ,read,update,productsCount} = require("../controllers/product");
+const { create,listAll,remove ,read,update,productsCount,productStar} = require("../controllers/product");
 const {list} = require("../controllers/product")
 // routes
 router.post("/product", authCheck, adminCheck, create);
@@ -15,5 +15,6 @@ router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug",read);
 router.put("/product/:slug",authCheck,adminCheck,update);
 router.post("/products", list);
-
+// rating
+router.put("/product/star/:productId", authCheck, productStar);
 module.exports = router;
